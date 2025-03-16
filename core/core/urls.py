13e7ccaa255d8523564af003362e1448ac99aa0d
Shaxzodbek <muxtorovshaxzodbek16@gmail.com>
2025-03-16 16:36:39 +0500

    Some changes

--- core/core/urls.py
+++ core/core/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 from core.settings import Config
 
@@ -10,6 +11,9 @@ config: Config = Config()
 urlpatterns = [
     path(f"{config.ADMIN_URL}/", admin.site.urls),
     path("", include("blog.urls")),
+    path('favicon.ico', RedirectView.as_view(url='/static/img/favicon.ico')),
+    path('apple-touch-icon.png', RedirectView.as_view(url='/static/img/apple-touch-icon.png')),
+    path('apple-touch-icon-precomposed.png', RedirectView.as_view(url='/static/img/apple-touch-icon-precomposed.png')),
 ]
 
 if settings.DEBUG:
